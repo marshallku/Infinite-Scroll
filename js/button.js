388bbd2722541ff4c883ddf18056af80e664ec50
Marshall Ku <taengoo890309@naver.com>
2020-03-03 22:37:46 +0900

    Create button.js

---
+++ js/button.js
@@ -0,0 +1,11 @@
+infiniteScroll({
+    container: "#container",
+    item: ".item",
+    next: ".next",
+    prev: ".prev",
+    nextButton: ".nextBtn",
+    prevButton: ".prevBtn",
+    nextLoader: ".nextLoader",
+    prevLoader: ".prevLoader",
+    pushHistory: true
+})
